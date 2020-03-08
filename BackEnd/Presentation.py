dccc9c57e722a404a778ab990676a44649e0c54d
jackbl1 <jackbloomfeld1@gmail.com>
2020-03-07 21:51:39 -0800

    Socketio

--- BackEnd/Presentation.py
+++ BackEnd/Presentation.py
@@ -1,3 +1,21 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('testEvent')
+def handle_test_event(arg1, arg2, arg3):
+    print('received args: ' + arg1 + arg2 + arg3)
+
 def present(sequences, players):
     for i in sequences:
         sequences[i].display(players[i])
